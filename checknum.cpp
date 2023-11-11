92e8b5c5b33ffb034c321e758e6166336aae3c43
huangyawen <huangyawen@iauto.com>
2023-11-11 16:27:10 +0800

    checksum

---
+++ checknum.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <cstdint>
+
+uint8_t calculate_checksum(uint16_t can_id, const std::vector<uint8_t>& data_bytes, uint8_t dlc) {
+    uint8_t checksum = 0;
+    
+    // Add the individual bytes of the CAN ID
+    checksum += (can_id & 0xFF); // Lower byte
+    checksum += ((can_id >> 8) & 0xFF); // Upper byte
+    
+    // Add the data bytes
+    for (uint8_t byte : data_bytes) {
+        checksum += byte;
+    }
+    
+    // Add the DLC
+    checksum += dlc;
+    
+    return checksum;
+}
+
+int main() {
+    uint16_t can_id = 0x294;
+    std::vector<uint8_t> data_bytes = {0x00, 0x00, 0x10, 0x00, 0x50, 0x00, 0x00, 0x00};
+    uint8_t dlc = 8;
+    
+    uint8_t checksum = calculate_checksum(can_id, data_bytes, dlc);
+    
+    std::cout << "Checksum: " << std::hex << +checksum << std::endl;
+    
+    return 0;
+}
